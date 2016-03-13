b280077e4b2427a44f2b2225aa7145da7e4286e3
wallase <wallase.huang@gmail.com>
2016-03-13 22:51:43 +0800

    customizedtimer add 'start' and 'stop' button 2016-03-13

--- customized_time.js
+++ customized_time.js
@@ -1,21 +1,42 @@
+var is_start = false;
+
+
 function minus() {
     if (document.getElementById("num")) {
         var curr = parseInt(document.getElementById("num").innerHTML);
     }
 
-    if (curr && curr <= 60) {
-
+    if (curr && curr <= 60 && is_start == true) {
         if (curr <= 10) {
+            num.style.color = "#EF233C";
             document.getElementById("num").innerHTML = "0" + (curr - 1);
+
+
         } else {
             document.getElementById("num").innerHTML = curr - 1;
         }
-    } else {
+    } else if (curr == 0) {
+        num.style.color = "#8D99AE";
         document.getElementById("num").innerHTML = 60;
     }
 
     setTimeout(function() {
         minus();
     }, 1000);
+
+
 };
 minus();
+
+function start() {
+    is_start = true;
+}
+
+function stop() {
+    is_start = false;
+}
+
+
+window.onload = function() {
+    start.style = "#08090d";
+}
